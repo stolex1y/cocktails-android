ed7d925cd4f5f15e0d361488d984e97a0efdcaca
Alexey Filimonov <filimonov.alks@gmail.com>
2023-08-08 10:38:43 +0300

    Add module config

---
+++ buildSrc/src/main/kotlin/modules/AppModuleConfig.kt
@@ -0,0 +1,7 @@
+package modules
+
+object AppModuleConfig : BaseModuleConfig() {
+    override val namespace = "ru.stolexiy.cocktails"
+    override val versionCode = 1
+    override val versionName = "1.0.0"
+}
---
+++ buildSrc/src/main/kotlin/modules/BaseModuleConfig.kt
@@ -0,0 +1,24 @@
+package modules
+
+import org.gradle.api.JavaVersion
+
+abstract class BaseModuleConfig {
+    abstract val namespace: String
+    abstract val versionCode: Int
+    abstract val versionName: String
+
+    open val compileSdk = 33
+    open val minSdk = 26
+    open val targetSdk = 33
+
+    open val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    open val proguardConsumerRules = "consumer-rules.pro"
+    open val testProguardRules = "consumer-rules.pro"
+    open val proguardRules = "proguard-rules.pro"
+    open val dimension = "environment"
+
+    open val sourceJdk = JavaVersion.VERSION_17
+    open val targetJdk = JavaVersion.VERSION_17
+
+    open val properties: Map<String, String> = emptyMap()
+}
